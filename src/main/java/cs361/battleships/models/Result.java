3a80898c4f6474edd870ae08c4fa7e803097a457
Max <harkindo@oregonstate.edu>
2018-10-11 17:40:32 -0700

    Removed Result files that were accidentally commited

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -2,45 +2,30 @@ package cs361.battleships.models;
 
 public class Result {
 
-	private Ship target;
-	private Square location;
-	private AttackStatus result;
-
-	// Default constructor. Must use getters and setters to initialize values if this is used.
-	public Result () {}
-
-	public Result (Square location, AttackStatus result, Ship target) {
-		// Set result location to passed location
-		this.location = location;
-
-		// Set result to passed result. Should be a valid attack status.
-		this.result = result;
-
-		// Sets the target to be the hit ship. If the attack had a MISS status, this should be NULL.
-		this.target = target;
-	}
-
 	public AttackStatus getResult() {
-		return result;
+		//TODO implement
+		return null;
 	}
 
 	public void setResult(AttackStatus result) {
-		this.result = result;
+		//TODO implement
 	}
 
 	public Ship getShip() {
-		return target;
+		//TODO implement
+		return null;
 	}
 
 	public void setShip(Ship ship) {
-		target = ship;
+		//TODO implement
 	}
 
 	public Square getLocation() {
-		return location;
+		//TODO implement
+		return null;
 	}
 
 	public void setLocation(Square square) {
-		location = square;
+		//TODO implement
 	}
 }
