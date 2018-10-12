324db25250093d0c1ed593b8de3844d841892dc1
Joey Landreville <16bitbow@gmail.com>
2018-10-11 18:46:44 -0700

    Implement basic Ship class features. (#4)

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -10,22 +10,31 @@ public class Ship {
 	@JsonProperty private List<Square> occupiedSquares;
 
 	private String kind;
+	private int size;
 
 	public Ship() {
 		occupiedSquares = new ArrayList<>();
 	}
-	
+
 	public Ship(String kind) {
         this.kind = kind;
-        occupiedSquares = new ArrayList<>();
-	}
 
-	public List<Square> getOccupiedSquares() {
-		//TODO implement
-		return null;
-	}
+        if (this.kind.equals("MINESWEEPER")) {
+            size = 2;
+        } else if (this.kind.equals("DESTROYER")) {
+            size = 3;
+        } else if (this.kind.equals("BATTLESHIP")) {
+            size = 4;
+        } else {
+            size = 0;
+        }
 
-	public String getKind() {
-	    return kind;
+        occupiedSquares = new ArrayList<>(size);
     }
+
+    public List<Square> getOccupiedSquares() { return occupiedSquares; }
+
+	public String getKind() { return kind; }
+
+    public int getSize() { return size; }
 }
--- src/test/java/cs361/battleships/models/ShipTest.java
+++ src/test/java/cs361/battleships/models/ShipTest.java
@@ -2,14 +2,35 @@ package cs361.battleships.models;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ShipTest {
 
     @Test
-    public void testKindAssignment()
+    public void testConstructor()
     {
-        Ship ship = new Ship("MINESWEEPER");
-        assertEquals(ship.getKind(), "MINESWEEPER");
+        // test kind setting works
+        Ship ship = new Ship("BATTLESHIP");
+        assertEquals(ship.getKind(), "BATTLESHIP");
+        // test getting occupiedSquares works
+        assertTrue(ship.getOccupiedSquares() instanceof List);
+        // test initial size of occupiedSquare
+        assertEquals(4, ship.getSize());
+    }
+
+    @Test
+    public void testOccupiedSquares()
+    {
+        Ship ship = new Ship("BATTLESHIP");
+
+        // test adding squares
+        ship.getOccupiedSquares().add(new Square(1, 'A'));
+        ship.getOccupiedSquares().add(new Square(1, 'B'));
+        ship.getOccupiedSquares().add(new Square(1, 'C'));
+        ship.getOccupiedSquares().add(new Square(1, 'D'));
+        assertEquals(4, ship.getOccupiedSquares().size());
     }
 }
